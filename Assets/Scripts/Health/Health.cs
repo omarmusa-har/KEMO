6204ded172fb7821fa3a9e79eac4f95ab2d684e4
omar musa <omarmusa200218@gmail.com>
2023-12-29 02:03:25 +0300

    Adding health bar and helath script with some errors

---
+++ Assets/Scripts/Health/Health.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+ 
+  [SerializeField] private float startingHealth;
+   public float currentHealth { get; private set; }
+     private void Awake()
+    {
+        currentHealth = startingHealth;
+       
+    }
+
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if (currentHealth > 0)
+        {
+           //pla
+        }
+        else
+        {
+          
+        }
+    
+    }
+
+     private void Update ()
+    {
+        if(Input.GetKeyDown(KetCode.E))
+        TakeDamage(1);
+    }
+
+}
