4d15433a493a8ca8d820816a9a3278d7ea60120c
omar musa <omarmusa200218@gmail.com>
2023-12-29 03:31:18 +0300

    finish the trap command and adding health

--- Assets/Scripts/Enemy_Sideways.cs
+++ Assets/Scripts/Enemy_Sideways.cs
@@ -15,7 +15,27 @@ public class Enemy_Sideways : MonoBehaviour
         leftEdge = transform.position.x - movementDistance;
         rightEdge = transform.position.x + movementDistance;
     }
-
+    private void Update()
+        {
+            if (movingLeft)
+            {
+                if (transform.position.x > leftEdge)
+                {
+                    transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
+                }
+                else
+                    movingLeft = false;
+            }
+            else
+            {
+                if (transform.position.x < rightEdge)
+                {
+                    transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+                }
+                else
+                    movingLeft = true;
+            }
+        }
      private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -4,39 +4,41 @@ public class Health : MonoBehaviour
 {
  
   [SerializeField] private float startingHealth;
-  private Animator anim;
-  private bool dead;
+    public float currentHealth { get; private set; }
+    private Animator anim;
+    private bool dead;
 
-
-   public float currentHealth { get; private set; }
-   
-     private void Awake()
+    private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
-       
     }
-
     public void TakeDamage(float _damage)
     {
         currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
 
         if (currentHealth > 0)
         {
-           anim.SetTrigger("hurt");
+            anim.SetTrigger("hurt");
+            //iframes
         }
         else
         {
-           if (!dead)
+            if (!dead)
             {
                 anim.SetTrigger("die");
                 GetComponent<PlayerMovement>().enabled = false;
                 dead = true;
             }
         }
-    
     }
+    public void AddHealth(float _value)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+    
+}
 
    
 
-}
+
