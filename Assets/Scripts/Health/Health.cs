3e45ca6b8e6aca141fe18314c798b5670a71adaf
omar musa <omarmusa200218@gmail.com>
2023-12-31 01:41:22 +0300

    add a enemy fireball and movement

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -1,9 +1,9 @@
-using System.Collections;
 using UnityEngine;
+using System.Collections;
 
 public class Health : MonoBehaviour
 {
-  [Header ("Health")]
+    [Header ("Health")]
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
     private Animator anim;
@@ -14,6 +14,10 @@ public class Health : MonoBehaviour
     [SerializeField] private int numberOfFlashes;
     private SpriteRenderer spriteRend;
 
+    [Header("Components")]
+    [SerializeField] private Behaviour[] components;
+    private bool invulnerable;
+
     private void Awake()
     {
         currentHealth = startingHealth;
@@ -22,6 +26,7 @@ public class Health : MonoBehaviour
     }
     public void TakeDamage(float _damage)
     {
+        if (invulnerable) return;
         currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
 
         if (currentHealth > 0)
@@ -34,17 +39,11 @@ public class Health : MonoBehaviour
             if (!dead)
             {
                 anim.SetTrigger("die");
-                //Player
-                if(GetComponent<PlayerMovement>() != null)
-                GetComponent<PlayerMovement>().enabled = false;
 
-                //Enemy
-                if(GetComponentInParent<EnemyPatrol>() != null)
-                GetComponentInParent<EnemyPatrol>().enabled = false;
+                //Deactivate all attached component classes
+                foreach (Behaviour component in components)
+                    component.enabled = false;
 
-                if(GetComponent<MeleeEnemy>() != null)
-                GetComponent<MeleeEnemy>().enabled = false;
-                
                 dead = true;
             }
         }
@@ -55,6 +54,7 @@ public class Health : MonoBehaviour
     }
     private IEnumerator Invunerability()
     {
+        invulnerable = true;
         Physics2D.IgnoreLayerCollision(10, 11, true);
         for (int i = 0; i < numberOfFlashes; i++)
         {
@@ -64,10 +64,6 @@ public class Health : MonoBehaviour
             yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
         }
         Physics2D.IgnoreLayerCollision(10, 11, false);
+        invulnerable = false;
     }
-    
 }
-
-   
-
-
